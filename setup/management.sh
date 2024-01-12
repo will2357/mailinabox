cade8e7cfaacfa51aa14f78d023d2065cc0cb708
will2357 <will.highducheck@gmail.com>
2024-01-12 14:58:09 -0500

    --ignore installed

--- setup/management.sh
+++ setup/management.sh
@@ -36,7 +36,7 @@ hide_output $venv/bin/pip install --upgrade pip
 # Install other Python 3 packages used by the management daemon.
 # The first line is the packages that Josh maintains himself!
 # NOTE: email_validator is repeated in setup/questions.sh, so please keep the versions synced.
-hide_output $venv/bin/pip install --upgrade \
+hide_output $venv/bin/pip install --upgrade --ignore-installed \
 	rtyaml "email_validator>=1.0.0" "exclusiveprocess" \
 	flask dnspython python-dateutil expiringdict gunicorn \
 	qrcode[pil] pyotp \
